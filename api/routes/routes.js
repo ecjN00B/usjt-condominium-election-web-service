d23cd7cc2cf843ec624e8ac18745dd41c288813d
Elias de Carvalho Junior <elias.de.carvalho@ibm.com>
2018-11-26 01:51:22 -0200

    Fix routes

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -8,14 +8,14 @@ module.exports = function(app) {
     app.route('/api/votes')
         .get(votes.list_all_votes)
         .post(votes.create_a_vote);
-    
-    app.route('/api/votes/:voterId')
-        .get(votes.read_a_vote)
-        .put(votes.update_a_vote)
-        .delete(votes.delete_a_vote);
 
     app.route('/api/candidates')
         .get(candidates.list_all_candidates)
         .post(candidates.create_a_candidate);
+    
+    app.route('/api/candidates/:candidateId')
+        .get(candidates.read_a_candidate)
+        .put(candidates.update_a_candidate)
+        .delete(candidates.delete_a_candidate);
 
 };
